13638767298bf5f1f9b6fca1d952a97a05bf4328
educauchy <educauchy@gmail.com>
2023-09-27 18:26:39 +0300

    ADD: tests for initialization and evaluation, Github Action for CI

---
+++ tests/test_experiment_evaluation.py
@@ -0,0 +1,233 @@
+from collections import Counter
+import numpy as np
+import pandas as pd
+
+import pytest
+
+from abacus.auto_ab.params import ABTestParams, DataParams, HypothesisParams
+from abacus.auto_ab import ABTest
+
+
+@pytest.fixture()
+def checks_dataframe() -> pd.DataFrame:
+    """
+    DataFrame with checks and sessions information
+    """
+    df = pd.read_csv("./tests/data/ab_data_check.csv")
+
+    return df
+
+
+@pytest.fixture()
+def continuous_test(checks_dataframe) -> ABTest:
+    """
+    Creation of simple continuous experiment
+    """
+    data_params = DataParams(
+        id_col="user_id",
+        group_col="groups",
+        control_name="control",
+        treatment_name="treatment",
+        target="check_rub_campaign",
+    )
+    hypothesis_params = HypothesisParams()
+
+    ab_params = ABTestParams(
+        data_params=data_params, hypothesis_params=hypothesis_params
+    )
+    ab_test = ABTest(checks_dataframe, ab_params)
+
+    return ab_test
+
+
+@pytest.fixture()
+def binary_test(checks_dataframe) -> ABTest:
+    """
+    Creation of simple binary experiment
+    """
+    data_params = DataParams(
+        id_col="user_id",
+        group_col="groups",
+        control_name="control",
+        treatment_name="treatment",
+        target_flg="has_transaction",
+    )
+    hypothesis_params = HypothesisParams(metric_type="binary")
+
+    ab_params = ABTestParams(
+        data_params=data_params, hypothesis_params=hypothesis_params
+    )
+    ab_test = ABTest(checks_dataframe, ab_params)
+
+    return ab_test
+
+
+@pytest.fixture()
+def binary_test_balanced(checks_dataframe) -> ABTest:
+    """
+    Creation of simple binary experiment with balanced groups
+    """
+    min_group_size = min(dict(Counter(checks_dataframe['groups'])).values())
+    control = checks_dataframe[checks_dataframe['groups'] == 'control'].sample(n=min_group_size).reset_index(drop=True)
+    treatment = checks_dataframe[checks_dataframe['groups'] == 'treatment'].sample(n=min_group_size).reset_index(drop=True)
+    checks_dataframe_balanced = pd.concat([control, treatment], axis=0)
+
+    data_params = DataParams(
+        id_col="user_id",
+        group_col="groups",
+        control_name="control",
+        treatment_name="treatment",
+        target_flg="has_transaction",
+    )
+    hypothesis_params = HypothesisParams(metric_type="binary")
+
+    ab_params = ABTestParams(
+        data_params=data_params, hypothesis_params=hypothesis_params
+    )
+    ab_test = ABTest(checks_dataframe_balanced, ab_params)
+
+    return ab_test
+
+
+@pytest.fixture()
+def ratio_test(checks_dataframe) -> ABTest:
+    """
+    Creation of simple ratio experiment
+    """
+    data_params = DataParams(
+        id_col="user_id",
+        group_col="groups",
+        control_name="control",
+        treatment_name="treatment",
+        numerator="clicks",
+        denominator="session_duration",
+    )
+    hypothesis_params = HypothesisParams(metric_type="ratio")
+
+    ab_params = ABTestParams(
+        data_params=data_params, hypothesis_params=hypothesis_params
+    )
+    ab_test = ABTest(checks_dataframe, ab_params)
+
+    return ab_test
+
+
+def test_evaluation_boot_confint_test(continuous_test):
+    """
+    Bootstrap confidence interval test evaluation
+    """
+    np.random.seed(42)
+    test_result = continuous_test.test_boot_confint()
+    true_test_result = {"stat": None, "p-value": 0.9999966831042315, "result": 1}
+
+    assert true_test_result == test_result
+
+
+def test_evaluation_boot_fp_test(continuous_test):
+    """
+    Bootstrap test evaluation using false positives
+    """
+    np.random.seed(42)
+    test_result = continuous_test.test_boot_fp()
+    true_test_result = {"stat": None, "p-value": 0, "result": 1}
+
+    assert true_test_result == test_result
+
+
+def test_evaluation_boot_ratio_test(ratio_test):
+    """
+    Bootstrap test evaluation for ratio metric
+    """
+    np.random.seed(42)
+    test_result = ratio_test.test_boot_ratio()
+    true_test_result = {"stat": None, "p-value": None, "result": 0}
+
+    assert true_test_result == test_result
+
+
+def test_evaluation_boot_welch_test(continuous_test):
+    """
+    Bootstrap test evaluation using Welch's t-test
+    """
+    np.random.seed(42)
+    test_result = continuous_test.test_boot_welch()
+    true_test_result = {"stat": None, "p-value": 0, "result": 1}
+
+    assert true_test_result == test_result
+
+
+def test_evaluation_buckets_test(continuous_test):
+    """
+    Welch's t-test on buckets of original data
+    """
+    np.random.seed(42)
+    test_result = continuous_test.test_buckets()
+    true_test_result = {"stat": None, "p-value": 2.1955744603423863e-06, "result": 1}
+
+    assert true_test_result == test_result
+
+
+def test_evaluation_chisquare_test(binary_test):
+    """
+    Chi-square test on imbalanced data: number of observations between groups is different
+    """
+    with pytest.raises(ValueError):
+        binary_test.test_chisquare()
+
+
+def test_evaluation_chisquare_test_balanced(binary_test_balanced):
+    """
+    Chi-square test on balanced data: number of observations between groups is equal
+    """
+    test_result = binary_test_balanced.test_chisquare()
+    true_test_result = {"stat": 1859.0982148148898, "p-value": 0, "result": 1}
+
+    assert true_test_result == test_result
+
+
+def test_evaluation_delta_ratio_test(ratio_test):
+    """
+    Delta method for evaluation of mean and variance of ratio metric and Welch's t-test for evaluation
+    """
+    test_result = ratio_test.test_delta_ratio()
+    true_test_result = {"stat": 0.6705474384264442, "p-value": None, "result": 0}
+
+    assert true_test_result == test_result
+
+
+def test_evaluation_mannwhitney_test(continuous_test):
+    """
+    Mann-Whitney's U-test
+    """
+    test_result = continuous_test.test_mannwhitney()
+    true_test_result = {
+        "stat": 4929788667,
+        "p-value": 5.3724037887497505e-08,
+        "result": 1,
+    }
+
+    assert true_test_result == test_result
+
+
+def test_evaluation_taylor_ratio_test(ratio_test):
+    """
+    Taylor series expansion for evaluation of mean and variance of ratio metric and Welch's t-test for evaluation
+    """
+    test_result = ratio_test.test_taylor_ratio()
+    true_test_result = {"stat": -20.063460617525994, "p-value": None, "result": 1}
+
+    assert true_test_result == test_result
+
+
+def test_evaluation_welch_test(continuous_test):
+    """
+    Welch's t-test
+    """
+    test_result = continuous_test.test_welch()
+    true_test_result = {
+        "stat": 4.784506533506825,
+        "p-value": 1.7152988957095832e-06,
+        "result": 1,
+    }
+
+    assert true_test_result == test_result
---
+++ tests/test_experiment_initialization.py
@@ -0,0 +1,176 @@
+import numpy as np
+import pandas as pd
+
+import pytest
+
+from abacus.auto_ab.params import ABTestParams, DataParams, HypothesisParams
+from abacus.auto_ab import ABTest
+
+
+@pytest.fixture()
+def checks_dataframe() -> pd.DataFrame:
+    """
+    DataFrame with checks and sessions information
+    """
+    df = pd.read_csv("./tests/data/ab_data_check.csv")
+
+    return df
+
+
+@pytest.mark.smoke
+def test_experiment_init_default(checks_dataframe):
+    """
+    Creation of experiment with default parameters
+    """
+    data_params = DataParams(
+        id_col="user_id", group_col="groups", target="check_rub_campaign"
+    )
+    hypothesis_params = HypothesisParams()
+
+    ab_params = ABTestParams(
+        data_params=data_params, hypothesis_params=hypothesis_params
+    )
+    ab_test = ABTest(checks_dataframe, ab_params)
+
+    return ab_test
+
+
+@pytest.mark.smoke
+def test_experiment_init_continuous(checks_dataframe):
+    """
+    Creation of simple continuous experiment
+    """
+    data_params = DataParams(
+        id_col="user_id",
+        group_col="groups",
+        control_name="control",
+        treatment_name="treatment",
+        target="check_rub_campaign",
+    )
+    hypothesis_params = HypothesisParams(metric_type="continuous")
+
+    ab_params = ABTestParams(
+        data_params=data_params, hypothesis_params=hypothesis_params
+    )
+    ab_test = ABTest(checks_dataframe, ab_params)
+
+    return ab_test
+
+
+@pytest.mark.smoke
+def test_experiment_init_binary(checks_dataframe):
+    """
+    Creation of simple binary experiment
+    """
+    data_params = DataParams(
+        id_col="user_id",
+        group_col="groups",
+        control_name="control",
+        treatment_name="treatment",
+        target_flg="has_transaction",
+    )
+    hypothesis_params = HypothesisParams(metric_type="binary")
+
+    ab_params = ABTestParams(
+        data_params=data_params, hypothesis_params=hypothesis_params
+    )
+    ab_test = ABTest(checks_dataframe, ab_params)
+
+    return ab_test
+
+
+@pytest.mark.smoke
+def test_experiment_init_ratio(checks_dataframe):
+    """
+    Creation of simple ratio experiment
+    """
+    data_params = DataParams(
+        id_col="user_id",
+        group_col="groups",
+        control_name="control",
+        treatment_name="treatment",
+        numerator="clicks",
+        denominator="session_duration",
+    )
+    hypothesis_params = HypothesisParams(metric_type="ratio")
+
+    ab_params = ABTestParams(
+        data_params=data_params, hypothesis_params=hypothesis_params
+    )
+    ab_test = ABTest(checks_dataframe, ab_params)
+
+    return ab_test
+
+
+@pytest.mark.smoke
+def test_experiment_init_continuous_custom_metric(checks_dataframe):
+    """
+    Creation of continuous experiment with custom metric
+    """
+
+    def quantile_95(x: np.ndarray) -> float:
+        return np.quantile(x, 0.95)
+
+    data_params = DataParams(
+        id_col="user_id",
+        group_col="groups",
+        control_name="control",
+        treatment_name="treatment",
+        target="check_rub_campaign",
+    )
+    hypothesis_params = HypothesisParams(
+        metric_type="continuous", metric_name="95th quantile", metric=quantile_95
+    )
+
+    ab_params = ABTestParams(
+        data_params=data_params, hypothesis_params=hypothesis_params
+    )
+    ab_test = ABTest(checks_dataframe, ab_params)
+
+    return ab_test
+
+
+@pytest.mark.smoke
+def test_experiment_init_transformations(checks_dataframe):
+    """
+    Creation of experiment with metric transformations
+    """
+    data_params = DataParams(
+        id_col="user_id",
+        group_col="groups",
+        control_name="control",
+        treatment_name="treatment",
+        target="check_rub_campaign",
+        transforms=["bucketing"],
+    )
+    hypothesis_params = HypothesisParams()
+
+    ab_params = ABTestParams(
+        data_params=data_params, hypothesis_params=hypothesis_params
+    )
+    ab_test = ABTest(checks_dataframe, ab_params)
+
+    return ab_test
+
+
+@pytest.mark.smoke
+def test_experiment_init_cuped(checks_dataframe):
+    """
+    Creation of experiment with variance reduction as CUPED
+    """
+    data_params = DataParams(
+        id_col="user_id",
+        group_col="groups",
+        control_name="control",
+        treatment_name="treatment",
+        target="check_rub_campaign",
+        covariate="check_rub_pre_campaign",
+    )
+    hypothesis_params = HypothesisParams()
+
+    ab_params = ABTestParams(
+        data_params=data_params, hypothesis_params=hypothesis_params
+    )
+    ab_test = ABTest(checks_dataframe, ab_params)
+
+    return ab_test
